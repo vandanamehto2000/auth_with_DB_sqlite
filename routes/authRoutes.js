7b708f0bcae3e99df4c4408edb2cadd3d80a76a0
vandana <vandana19@navgurukul.org>
2022-12-20 14:22:08 +0530

    comment on some codes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); //bcrypt is a 3rd party library which we use to hash, salt and compare passwords
 const createDB = require("../config/db");
 const User = require("../models/userModels");
 
 const { validateName, validateEmail, validatePassword } = require("../utils/validators");
 
-createDB.sync().then(() => {
+createDB.sync().then(() => { //Sync all defined models to the DB
     console.log("database is running");
 });
 
@@ -15,15 +15,10 @@ router.post("/signup", async (req, res) => {
     try {
         const { name, email, password } = req.body; //destructuring name, email and password out of the request body
         console.log(name, email, password);
-        const userExist = await User.findOne({
-            where: {
-                email
-            }
-        })
+        const userExist = await User.findOne({where: {email}}) //check if the user with the entered email already exists in the database
 
         if (userExist) {
-            return res.status(403).send("User already exists"); //check if the user with the entered email already exists in the database
-        }
+            return res.status(403).send("User already exists");        }
 
         if (!validateName(name)) {
             // res.send("Invalid name")
@@ -38,7 +33,7 @@ router.post("/signup", async (req, res) => {
             return res.status(400).send("Invalid password: password must be at least 8 characters long and must include atlest one - one uppercase letter, one lowercase letter, one digit, one special character");
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 10); //hashes the password with a salt, generated with the specified number of rounds
 
         const saveToDB = {
             name, email, password: hashedPassword
